09aac2542e6c7bdd5176a2e641566e85ebc20110
holotr <holotr@users.noreply.github.com>
2023-01-02 17:40:44 +0800

    Update diy-part2.sh

--- device/rm-ax6000/diy-part2.sh
+++ device/rm-ax6000/diy-part2.sh
@@ -31,4 +31,4 @@ sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_
 rm -rf package/feeds/holotr/my-default-settings/files/usr
 sed -i "s/%D %V, %C/%D %C by Tau'ri/" package/base-files/files/etc/banner
 sed -i "s/%D %V %C/%D %C by Tau'ri/" package/base-files/files/etc/openwrt_release
-curl -sfL https://github.com/holotr/opw/raw/master/device/rm-ax6000/patch/base-file.patch | patch -d './' -p1 --forward" || true
+curl -sfL https://github.com/holotr/opw/raw/master/device/rm-ax6000/patch/base-file.patch || patch -d './' -p1 --forward" || true
